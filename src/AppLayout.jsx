fac79b343ac160999c3be9c281719b9c9615843d
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-01-30 09:31:16 +0530

    refactor: Update AppLayout styling for improved visual consistency

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -10,8 +10,8 @@ import { DataProvider } from "./Contexts/DataWhereHouse";
 
   return (
     <>
-      <main className={`cont h-lvh w-lvw grid ${sideBarClass} grid-rows-[0.3fr_0.3fr_3fr_0.2fr] gap-2  relative`}>
-        <nav className="head row-start-1 row-end-2 col-start-2 col-end-4 bg-yellow-100">
+      <main className={`cont h-lvh w-lvw grid ${sideBarClass} grid-rows-[0.3fr_0.3fr_3fr_0.2fr] gap-2  relative bg-neutral-200`}>
+        <nav className="head row-start-1 row-end-2 col-start-2 col-end-4 bg-white">
           <h2 className="text-4xl font-semibold">Hello, Debanajan Bera</h2>
           <h3 className="text-3xl font-semibold">My Task</h3>
         </nav>
