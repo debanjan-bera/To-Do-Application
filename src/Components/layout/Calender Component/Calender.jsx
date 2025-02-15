65ca88b0005b9206c6e8de3acc00cc3b540239c9
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-02-15 21:19:56 +0530

    refactor: Optimize active date color logic in CalenderComponent and enhance Sunday styling

--- src/Components/layout/Calender Component/Calender.jsx
+++ src/Components/layout/Calender Component/Calender.jsx
@@ -4,7 +4,9 @@ import { useState } from "react";
 import { IoMdArrowDropleftCircle, IoMdArrowDroprightCircle } from "react-icons/io";
 export const CalenderComponent = () => {
   const [selectedDate, setSelectedDate] = useState(null);
-  const currentDate = new Date();
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  // const currentDate = new Date();
   const [currentMonth,setCurrentMonth] = useState(currentDate.getMonth())
   const [currentYear,setCurrentYear] = useState(currentDate.getFullYear())
   const listOfMonth = ["January", "February", "March", "April", "May", "June", 
@@ -12,7 +14,6 @@ export const CalenderComponent = () => {
   const listOfDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
   const daysInMonth = new Date(currentYear,currentMonth + 1,0).getDate()
   const fastDayOfMonth = new Date(currentYear,currentMonth , 1).getDay()
-  console.log(new Date('2025-02-14'),daysInMonth,fastDayOfMonth);
   const handleEventCalender = (index) => {
     setSelectedDate(index);  // Store the selected date index
   };
@@ -30,20 +31,22 @@ export const CalenderComponent = () => {
     }
     return sundays;
   };
+  const handelPrevMonth = () =>{
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
+  }
+  const handelNextMonth = () => setCurrentDate(new Date(currentDate.getFullYear(), setCurrentMonth(currentMonth+1), 1));
 
 // Example usage:
 // styling elements
-
 const activeDateColor = (date) => {
-  const sundays = checkOnSunday(currentYear, currentMonth);
   const actualDate = date + 1;
+  const sundays = checkOnSunday(currentYear, currentMonth);
+  const isSunday = sundays.includes(actualDate)
   if (currentDate.getDate() === actualDate) return "text-white bg-blue-400 hover:bg-blue-500"; // Default today styling (unclicked)
-  // else if(sundays.includes(actualDate)) return 
   else if(selectedDate === date){
-    if (sundays.includes(actualDate)) return "bg-red-100 text-red-500"
-    return "text-white bg-sky-500"; // Selected date (including current date when clicked)
+    return isSunday ? "bg-red-100 text-red-500" : "text-white bg-sky-400"; 
   }
-  if (sundays.includes(actualDate)) return "text-red-500 hover:bg-red-100"
+  if (isSunday) return "text-red-500 hover:bg-red-100"
   return "text-black hover:bg-sky-100"; // Normal styling for other dates
 };
 
@@ -51,18 +54,7 @@ const activeDateColor = (date) => {
   return (
     <>
       <aside className="calender bg-[#1E1F24] row-start-2 row-end-5 col-start-3 text-white text-base flex flex-col  items-center">
-        <section className="bg-black/10  fill-transparent w-[3.3rem]">
-          <label className="hamburger cursor-pointer ">
-            <input type="checkbox" className="hidden" />
-            <svg viewBox="0 0 32 32" className="h-[3em]">
-              <path
-                className="line line-top-bottom"
-                d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
-              ></path>
-              <path className="line" d="M7 16 27 16"></path>
-            </svg>
-          </label>
-        </section>
+
         <section className="w-[94%] bg-white grid  text-black p-2 rounded-md">
           <section className="w-full py-4 flex flex-row justify-between items-center">
             <div className="text-[1.8rem] flex gap-1">
@@ -71,8 +63,8 @@ const activeDateColor = (date) => {
               <div>{currentYear}</div>
             </div>
             <div className="flex gap-1 text-[1.8rem]">
-              <button><IoMdArrowDropleftCircle /></button>
-              <button><IoMdArrowDroprightCircle /></button> 
+              <button onClick={handelPrevMonth}><IoMdArrowDropleftCircle /></button>
+              <button onClick={handelNextMonth}><IoMdArrowDroprightCircle /></button> 
             </div>
           </section>
           <div className="grid grid-cols-7 gap-1 text-center">
@@ -91,7 +83,7 @@ const activeDateColor = (date) => {
               <span
                 key={day + 1}
                 onClick={() => handleEventCalender(day)}
-                className={`text-lg font-semibold p-[0.3rem] rounded-full border border-white scale-100 transition-all
+                className={`text-lg font-semibold p-[0.3rem] rounded-full border border-white scale-100 cursor-pointer select-none transition-all
                 ${activeDateColor(day)} `}>
                 {day + 1}
               </span>
