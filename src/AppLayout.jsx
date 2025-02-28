6d9cef05d9a87de1c8ba5adb7492753098601c11
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-02-28 23:59:14 +0530

    refactor: Update AppLayout to use full screen height for improved layout consistency

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -18,7 +18,7 @@ export const AppLayout = () => {
   return (
     <DataProvider>
       <section
-        className={`h-lvh w-lvw relative bg-[#1E1F24] grid ${sideBarClass} grid-rows-[0.3fr_0.3fr_3fr]`}
+        className={`h-screen w-lvw relative bg-[#1E1F24] grid ${sideBarClass} grid-rows-[0.3fr_0.3fr_3fr]`}
       >
         <header className="head row-start-1 row-end-2 col-start-2 col-end-4 p-4 pb-2">
           <div className="text-2xl font-medium text-white flex flex-row items-center justify-start gap-3">
