0b7f6248e6b85bc79e5bf9ab4b7cb2affb28514f
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-03-10 22:50:54 +0530

    refactor: Update Date component greeting format and improve icon styling for better presentation

--- src/Components/Functions/Date.jsx
+++ src/Components/Functions/Date.jsx
@@ -6,17 +6,16 @@ export const DateAndTime = () => {
       ? { greeting: "Good Morning", icon: <WiSunrise className="text-yellow-400 text-4xl pl-1" /> }
       : hour >= 12 && hour < 18
       ? { greeting: "Good Afternoon!!", icon: '☀️' }
-      : { greeting: "Good Evening!!", icon: <WiNightClear className="text-blue-400 text-4xl" /> };
+      : { greeting: "Good Evening,", icon: <WiNightClear className="text-blue-400 text-4xl" /> };
 
   return (
     <div className="flex items-center flex-wrap">    
       <div className="h-full flex items-center justify-center ">
-        <div className="h-full rounded-full p-[0.1rem] bg-neutral-700 mr-1 flex items-center justify-center text-center">{icon}</div>
-        <h1>
-        Debanjan Bera
-        </h1>
+        <div className="h-full rounded-full p-[0.1rem] bg-black border-[0.02rem] border-neutral-600 mr-3 flex items-center justify-center text-center">{icon}</div>
+        
         </div>
-      <h1 className=" text-center">{greeting}</h1>
+        
+      <h1 className=" text-center">{`${greeting} Debanjan Bera`}</h1>
     </div>
   );
 };
