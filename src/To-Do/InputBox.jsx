342d11389ccdf5ce8c787a25850b190e41e1b146
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-03-16 23:46:13 +0530

    refactor: Clean up AddTaskForm component by removing unused code and improving formatting for better readability

--- src/To-Do/InputBox.jsx
+++ src/To-Do/InputBox.jsx
@@ -31,11 +31,7 @@ export const AddTaskForm = () => {
     
     handleFromSubmit(finalData, taskArr, setTaskArr, setWindowClose, setmobileAddButton);
   };
-  // useEffect(() => {
-  //     const handleClickOutside = () => setWindowClose(false);
-  //     document.addEventListener("click", handleClickOutside);
-  //     return () => document.removeEventListener("click", handleClickOutside);
-  //   }, [setWindowClose]);
+
     useEffect(() => {
       const handleClickOutside = (event) => {
         if (modalRef.current && !modalRef.current.contains(event.target)) {
@@ -48,35 +44,19 @@ export const AddTaskForm = () => {
     }, [setWindowClose]);
   return (
     <section className="register-cont w-lvw h-dvh absolute top-0 left-0 bg-black/70 z-10 flex items-center justify-center">
-      <motion.form
-        ref={modalRef}
-        className="bg-neutral-100 p-4 rounded-md"
-        initial={{ opacity: 0, y: 25 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 25 }}
-        drag="y"
-        dragControls={controls}
-        dragConstraints={{ top: 0, bottom: 0 }}
-        onSubmit={handleSubmit(onSubmit)}
-      >
+      <motion.form ref={modalRef} className="bg-neutral-100 p-4 rounded-md" initial={{ opacity: 0, y: 25 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 25 }} drag="y" dragControls={controls} dragConstraints={{ top: 0, bottom: 0 }} onSubmit={handleSubmit(onSubmit)}>
         <label className="mb-2 flex items-center justify-center">
           <div className="w-14 h-2 bg-neutral-400 rounded-full cursor-grab active:cursor-grabbing"></div>
         </label>
-
         <label>
           <h1 className="text-3xl font-bold">Create new task...</h1>
         </label>
 
         <label>
           <p className="text-xl font-bold py-1">Task:</p>
-          <input
-            type="text"
-            placeholder="Add your important task..."
-            autoFocus
+          <input type="text" placeholder="Add your important task..." autoFocus
             className="w-[22rem] p-[0.5rem] outline-none text-xl rounded border-[1.5px] border-gray-400"
-            autoComplete="off"
-            {...register("content", { required: true })}
-          />
+            autoComplete="off" {...register("content", { required: true })}/>
           <p className="h-6 text-base text-red-600">
             {errors.content && <span>This field is required</span>}
           </p>
@@ -87,8 +67,7 @@ export const AddTaskForm = () => {
             <select
               {...register("group")}
               className="w-[22rem] p-[0.5rem] text-lg rounded outline-none border-[1.5px] border-gray-400"
-              onChange={(e) => setSelectedGroup(e.target.value)}
-            >
+              onChange={(e) => setSelectedGroup(e.target.value)}>
               {groupData.map((e, index) => (
                 <option key={index} value={e}>
                   {e}
@@ -100,12 +79,9 @@ export const AddTaskForm = () => {
         {selectedGroup === "Others" && (
           <label>
             <p className="text-xl font-bold py-1">Custom Group Name:</p>
-            <input
-              type="text"
-              placeholder="Enter custom group name..."
+            <input type="text" placeholder="Enter custom group name..."
               className="w-[22rem] p-[0.5rem] outline-none text-xl rounded border-[1.5px] border-gray-400"
-              value={customGroup}
-              onChange={(e) => setCustomGroup(e.target.value)}
+              value={customGroup} onChange={(e) => setCustomGroup(e.target.value)}
             />
           </label>
         )}
@@ -128,12 +104,10 @@ export const AddTaskForm = () => {
           <button
             type="button"
             className="w-24 h-10 my-2 mr-2 bg-white border-2 border-black text-lg font-semibold"
-            onClick={() => handleFormCancel(setWindowClose, setmobileAddButton)}
-          >
+            onClick={() => handleFormCancel(setWindowClose, setmobileAddButton)}>
             Cancel
           </button>
-          <button
-            type="submit"
+          <button type="submit"
             className="button-submit w-24 h-10 bg-black my-2 text-white text-lg font-semibold border-2 border-black cursor-pointer">
             Save
           </button>
