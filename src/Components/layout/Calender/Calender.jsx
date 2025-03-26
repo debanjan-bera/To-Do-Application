b298878bcebb846e6ad96b060b0bad397a0207d8
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-03-26 22:53:47 +0530

    refactor: Clean up CalenderComponent code formatting and improve layout structure

--- src/Components/layout/Calender/Calender.jsx
+++ src/Components/layout/Calender/Calender.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
-import { IoMdArrowDropleftCircle, IoMdArrowDroprightCircle } from "react-icons/io";
+import {
+  IoMdArrowDropleftCircle,
+  IoMdArrowDroprightCircle,
+} from "react-icons/io";
 
 export const CalenderComponent = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
@@ -7,8 +10,20 @@ export const CalenderComponent = () => {
   const [isSunday, setSunday] = useState([]);
 
   const listOfDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const listOfMonths = ["January", "February", "March", "April", "May", "June", 
-    "July", "August", "September", "October", "November", "December"];
+  const listOfMonths = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth();
@@ -16,19 +31,28 @@ export const CalenderComponent = () => {
   const lastDay = new Date(year, month + 1, 0).getDate();
   const prevMonthLastDay = new Date(year, month, 0).getDate();
 
-  const prevMonthDates = Array.from({ length: firstDay }, (_, i) => prevMonthLastDay - firstDay + i + 1);
+  const prevMonthDates = Array.from(
+    { length: firstDay },
+    (_, i) => prevMonthLastDay - firstDay + i + 1
+  );
 
-  const totalCells = (Math.ceil((firstDay + lastDay) / 7) * 7) - (firstDay + lastDay);
+  const totalCells =
+    Math.ceil((firstDay + lastDay) / 7) * 7 - (firstDay + lastDay);
   const nextMonthDays = Array.from({ length: totalCells }, (_, i) => i + 1);
 
-  const flexClass = 'flex items-center justify-center';
-  const disableDayClass = 'p-1 w-full h-full aspect-square text-gray-400 rounded-full hover:bg-[#27272a]';
+  const flexClass = "flex items-center justify-center";
+  const disableDayClass =
+    "p-1 w-full h-full aspect-square text-gray-400 rounded-full hover:bg-[#27272a]";
 
   const trackTodayDate = (date) => {
-    if (todayDate.getDate() === date && todayDate.getMonth() === month && todayDate.getFullYear() === year) {
-      return 'border text-blue-700 font-bold bg-white  hover:bg-white';
+    if (
+      todayDate.getDate() === date &&
+      todayDate.getMonth() === month &&
+      todayDate.getFullYear() === year
+    ) {
+      return "border text-blue-700 font-bold bg-white  hover:bg-white";
     }
-    return isSunday.includes(date) ? 'text-red-500' : 'text-white';
+    return isSunday.includes(date) ? "text-red-500" : "text-white";
   };
 
   const prevMonth = () => setCurrentDate(new Date(year, month - 1, 1));
@@ -44,53 +68,68 @@ export const CalenderComponent = () => {
 
   return (
     <>
-      <aside className='calender bg-[#0B0D0E] border-l-[0.02rem] border-neutral-700 justify-items-center row-start-2 row-end-5 col-start-3 w-full grid'>
-        <section className='h-full w-[20rem]'>
-          <div className="p-2 w-[102%] pt-2 bg-white/10 text-white text-xl"
-          onClick={()=>{
-            if (currentDate.getFullYear() !== todayDate.getFullYear() || currentDate.getMonth() !== todayDate.getMonth()){
-              setCurrentDate(new Date(todayDate.getFullYear(),todayDate.getMonth(), 1))
-              console.log('hello');
-            }
-             
-            else return
-
+      <aside className="calender bg-[#0B0D0E] border-l-[0.02rem] border-neutral-700 row-start-2 row-end-5 col-start-3 flex flex-col items-center">
+        <div
+          className="p-2  w-full pt-2 bg-white/10 text-white text-xl"
+          onClick={() => {
+            if (currentDate.getFullYear() !== todayDate.getFullYear() ||currentDate.getMonth() !== todayDate.getMonth())
+              setCurrentDate( new Date(todayDate.getFullYear(), todayDate.getMonth(), 1));
+            else return;
           }}
-          >
-            {`${listOfDays[todayDate.getDay()]}, ${todayDate.getDate()} ${listOfMonths[todayDate.getMonth()]}`}
-          </div>
+        >
+          {`${listOfDays[todayDate.getDay()]}, ${todayDate.getDate()} ${listOfMonths[todayDate.getMonth()]}`}
+        </div>
+        <section className="w-[20rem]">
           <div className="w-full p-1 text-white flex items-center justify-between text-center">
             <span className="text-3xl p-1 text-center">{`${listOfMonths[month]}, ${year}`}</span>
             <div className="text-3xl p-1 text-center">
-              <button onClick={prevMonth}><IoMdArrowDropleftCircle /></button>
-              <button onClick={nextMonth}><IoMdArrowDroprightCircle /></button>
+              <button onClick={prevMonth}>
+                <IoMdArrowDropleftCircle />
+              </button>
+              <button onClick={nextMonth}>
+                <IoMdArrowDroprightCircle />
+              </button>
             </div>
           </div>
 
-          <section className='w-full h-[20.4rem] text-lg text-white grid grid-cols-7 grid-rows-7 gap-1'>
+          <section className="w-full h-[20.4rem] text-lg text-white grid grid-cols-7 grid-rows-7 gap-1">
             {listOfDays.map((day) => (
-              <div key={day} className={`p-1 ${flexClass}`}>{day}</div>
+              <div key={day} className={`p-1 ${flexClass}`}>
+                {day}
+              </div>
             ))}
 
             {prevMonthDates.map((day, index) => (
               <div key={`prev-${index}`} className="">
-                <div className={`${disableDayClass} ${flexClass}`}>
-                {day}
-                </div>
+                <div className={`${disableDayClass} ${flexClass}`}>{day}</div>
               </div>
             ))}
 
-            {Array.from({ length: lastDay }, (_, i) => i + 1).map((day, index) => (
-              <div key={index} className={`w-full h-full p-1 rounded-full ${flexClass} aspect-square ${trackTodayDate(day)}  hover:bg-[#27272a]`}> 
-                {day}
-              </div>
-            ))}
+            {Array.from({ length: lastDay }, (_, i) => i + 1).map(
+              (day, index) => (
+                <div
+                  key={index}
+                  className={`w-full h-full p-1 rounded-full ${flexClass} aspect-square ${trackTodayDate(
+                    day
+                  )}  hover:bg-[#27272a]`}
+                >
+                  {day}
+                </div>
+              )
+            )}
 
             {nextMonthDays.map((day, index) => (
-              <div key={`prev-${index}`} className={`${disableDayClass} ${flexClass}`}>{day}</div>
+              <div
+                key={`prev-${index}`}
+                className={`${disableDayClass} ${flexClass}`}
+              >
+                {day}
+              </div>
             ))}
           </section>
         </section>
+        <div className="h-[80%] w-full bg-white"> hello</div>
+        <div className="h-[20%] w-full bg-red-500"> hello</div>
       </aside>
     </>
   );
