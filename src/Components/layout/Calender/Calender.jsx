12408d7f481721162883aaef83843b4c660b70cf
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-03-27 15:59:02 +0530

    refactor: Simplify CalenderComponent logic and enhance styles for improved accessibility

--- src/Components/layout/Calender/Calender.jsx
+++ src/Components/layout/Calender/Calender.jsx
@@ -10,20 +10,7 @@ export const CalenderComponent = () => {
   const [isSunday, setSunday] = useState([]);
 
   const listOfDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const listOfMonths = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  const listOfMonths = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth();
@@ -31,13 +18,9 @@ export const CalenderComponent = () => {
   const lastDay = new Date(year, month + 1, 0).getDate();
   const prevMonthLastDay = new Date(year, month, 0).getDate();
 
-  const prevMonthDates = Array.from(
-    { length: firstDay },
-    (_, i) => prevMonthLastDay - firstDay + i + 1
-  );
+  const prevMonthDates = Array.from({ length: firstDay },(_, i) => prevMonthLastDay - firstDay + i + 1);
+  const totalCells = Math.ceil((firstDay + lastDay) / 7) * 7 - (firstDay + lastDay);
 
-  const totalCells =
-    Math.ceil((firstDay + lastDay) / 7) * 7 - (firstDay + lastDay);
   const nextMonthDays = Array.from({ length: totalCells }, (_, i) => i + 1);
 
   const flexClass = "flex items-center justify-center";
@@ -52,12 +35,12 @@ export const CalenderComponent = () => {
     ) {
       return "border text-blue-700 font-bold bg-white  hover:bg-white";
     }
-    return isSunday.includes(date) ? "text-red-500" : "text-white";
+    return isSunday.includes(date) ? "text-red-500 hover:bg-red-900/30" : "text-white";
   };
 
   const prevMonth = () => setCurrentDate(new Date(year, month - 1, 1));
   const nextMonth = () => setCurrentDate(new Date(year, month + 1, 1));
-
+  
   useEffect(() => {
     const sundays = [];
     for (let day = 1; day <= lastDay; day++) {
@@ -66,6 +49,7 @@ export const CalenderComponent = () => {
     setSunday(sundays);
   }, [year, month, lastDay]);
 
+  // useEffect(()=>{console.log(currentDate,month,year)},[currentDate,month,year])
   return (
     <>
       <aside className="calender bg-[#0B0D0E] border-l-[0.02rem] border-neutral-700 row-start-2 row-end-5 col-start-3 flex flex-col items-center">
@@ -92,7 +76,7 @@ export const CalenderComponent = () => {
             </div>
           </div>
 
-          <section className="w-full h-[20.4rem] text-lg text-white grid grid-cols-7 grid-rows-7 gap-1">
+          <section className="w-full h-[20.4rem] mb-1 text-lg text-white grid grid-cols-7 grid-rows-7 gap-1">
             {listOfDays.map((day) => (
               <div key={day} className={`p-1 ${flexClass}`}> {day} </div> ))}
 
@@ -104,15 +88,16 @@ export const CalenderComponent = () => {
 
             {Array.from({ length: lastDay }, (_, i) => i + 1).map((day, index) => (
                 <div key={index}
-                  className={`w-full h-full p-1 rounded-full ${flexClass} aspect-square ${trackTodayDate(day)}  hover:bg-[#27272a]`}>
+                  className={`w-full h-full p-1 rounded-full ${flexClass} aspect-square ${trackTodayDate(day)}  hover:bg-[#27272a]`}
+                  onClick={()=>{console.log(day,month+1,year)}}>
                   {day}
                 </div>))}
 
             {nextMonthDays.map((day, index) => (<div key={`prev-${index}`} className={`${disableDayClass} ${flexClass}`}> {day} </div>))}
           </section>
         </section>
-        <div className="h-[80%] w-full border-y  overflow-y-scroll"> hello</div>
-        <div className="h-[20%] w-full bg-[#212121]"> hello</div>
+        <div className="h-[80%] w-full border-y text-white overflow-y-scroll"> hello</div>
+        <div className="h-[20%] w-full bg-[#212121] text-white"> hello</div>
       </aside>
     </>
   );
