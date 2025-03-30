352182614a65b9744be68eb936528dc9b2acfcd3
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-03-30 22:44:55 +0530

    refactor: Update AboutModel layout for improved structure and styling

--- src/Components/Functions/Models/TaskAboutModel.jsx
+++ src/Components/Functions/Models/TaskAboutModel.jsx
@@ -3,7 +3,11 @@ export const AboutModel = ()=>{
     return(
         <>
             <div className="w-full h-full bg-[#010101] absolute top-0 left-0 text-white">
-                <div className="m-2 text-xl flex justify-end"><RxCross1 /></div>
+                <div className="m-2  flex justify-between items-center ">
+                    <p className="pb-1 text-3xl border-b">About</p>
+                    <div className="p-2 text-2xl rounded-full hover:text-white/80 hover:bg-neutral-700"><RxCross1 /></div>
+                
+                </div>
                 <div>
                     <p className="m-2 py-2 text-2xl">Task</p>
                     <p className="m-2 p-2 text-xl bg-[#1A1A1A] border border-neutral-600 rounded ">hello</p>
