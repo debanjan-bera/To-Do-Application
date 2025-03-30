3a45b21a626240213d2d71582f61b5a8ad1584eb
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-03-30 16:20:16 +0530

    refactor: Clean up code formatting and improve readability in CalenderComponent

--- src/Components/layout/Calender/Calender.jsx
+++ src/Components/layout/Calender/Calender.jsx
@@ -1,16 +1,15 @@
-
 import { useEffect, useState } from "react";
-import { IoMdArrowDropleftCircle, IoMdArrowDroprightCircle } from "react-icons/io";
+import {IoMdArrowDropleftCircle,IoMdArrowDroprightCircle,
+} from "react-icons/io";
 
 export const CalenderComponent = () => {
-  const [currentDate,setCurrentDate] = useState(new Date())
+  const [currentDate, setCurrentDate] = useState(new Date());
   const [isSunday, setSunday] = useState([]);
   const [selectedDate, setSelectedDate] = useState(null);
-  const [targetDate,setTargetDate] = useState()
-  const todayDate = new Date()
+  const [targetDate, setTargetDate] = useState();
+  const todayDate = new Date();
   const listOfDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  const listOfMonths = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"];
+  const listOfMonths = ["January","February","March","April","May","June","July","August","September","October","November","December",];
 
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth();
@@ -18,18 +17,19 @@ export const CalenderComponent = () => {
   const lastDay = new Date(year, month + 1, 0).getDate();
   const prevMonthLastDay = new Date(year, month, 0).getDate();
 
-  const prevMonthDates = Array.from({ length: firstDay }, (_, i) => prevMonthLastDay - firstDay + i + 1);
-  const totalCells = Math.ceil((firstDay + lastDay) / 7) * 7 - (firstDay + lastDay);
+  const prevMonthDates = Array.from( { length: firstDay }, (_, i) => prevMonthLastDay - firstDay + i + 1 );
+  const totalCells =
+    Math.ceil((firstDay + lastDay) / 7) * 7 - (firstDay + lastDay);
   const nextMonthDays = Array.from({ length: totalCells }, (_, i) => i + 1);
 
   const trackTodayDate = (date) => {
-    const isToday = todayDate.getDate() === date && month === todayDate.getMonth() && year === todayDate.getFullYear();
-    const isTargetDate = targetDate === date;
+    const isToday =todayDate.getDate() === date &&month === todayDate.getMonth() &&year === todayDate.getFullYear();
+    const isTargetDate =targetDate === date &&month === todayDate.getMonth() &&year === todayDate.getFullYear();
     const isSundayDate = isSunday.includes(date);
     if (isToday) return "border text-blue-700 font-bold bg-white hover:bg-white";
     if (isSundayDate && !isTargetDate) return "text-red-500 hover:bg-red-900/30";
-    if (isTargetDate && isSundayDate) return 'bg-red-900/30 border border-red-800 text-red-500 hover:bg-red-900/30 '
-    if (isTargetDate) return "bg-sky-300 text-blue-900 font-extrabold hover:bg-sky-400";
+    if (isTargetDate && isSundayDate) return "bg-red-900/30 border border-red-800 text-red-500 hover:bg-red-900/30 ";
+    if (isTargetDate) return "bg-blue-800/20 text-blue-300 border border-blue-700 hover:bg-blue-800/40";
     return "text-white";
   };
 
@@ -39,35 +39,31 @@ export const CalenderComponent = () => {
   const disableDayClass = "text-gray-400 rounded-full hover:bg-[#27272a]";
   const flexClass = "flex items-center justify-center";
 
-
   const handleDateClick = (e) => {
     const clickedElement = e.target;
 
     if (!clickedElement.classList.contains("current-month")) return; // Only allow clicks on current month days
     const day = parseInt(clickedElement.textContent);
-    setTargetDate(day)
-    if(selectedDate ===  `${day} ${listOfMonths[month]} ${year}`) return
+    setTargetDate(day);
+    if (selectedDate === `${day} ${listOfMonths[month]} ${year}`) return;
 
     if (!isNaN(day)) {
       const formattedDate = `${day} ${listOfMonths[month]} ${year}`;
       setSelectedDate(formattedDate);
       console.log(`Selected Date: ${formattedDate}`);
     }
-    // console.log(targetDate);
   };
 
   useEffect(() => {
     const sundays = [];
-    for (let day = 1; day <= lastDay; day++) {
-      if (new Date(year, month, day).getDay() === 0) sundays.push(day);
-    }
+    for (let day = 1; day <= lastDay; day++) {if (new Date(year, month, day).getDay() === 0) sundays.push(day);}
     setSunday(sundays);
   }, [year, month, lastDay]);
 
   return (
-    <aside className="calender bg-[#0B0D0E] border-l-[0.02rem] border-neutral-700 row-start-2 row-end-5 col-start-3 flex flex-col items-center">
+    <>
       <section className="w-full font-bold border-b border-gray-500 text-white text-xl cursor-pointer"
-        onClick={() => setCurrentDate(new Date(todayDate.getFullYear(), todayDate.getMonth(), 1))}>
+        onClick={() =>setCurrentDate(new Date(todayDate.getFullYear(), todayDate.getMonth(), 1))}>
         <div className="p-3 hover:bg-white/10">
           {`${listOfDays[todayDate.getDay()]}, ${todayDate.getDate()} ${listOfMonths[todayDate.getMonth()]}`}
         </div>
@@ -76,49 +72,44 @@ export const CalenderComponent = () => {
         <div className="w-full p-1 text-white flex items-center justify-between text-center">
           <span className="text-3xl p-1 text-center">{`${listOfMonths[month]}, ${year}`}</span>
           <div className="text-3xl p-1 text-center">
-            <button onClick={prevMonth}><IoMdArrowDropleftCircle /></button>
-            <button onClick={nextMonth}><IoMdArrowDroprightCircle /></button>
+            <button onClick={prevMonth}> <IoMdArrowDropleftCircle /> </button>
+            <button onClick={nextMonth}> <IoMdArrowDroprightCircle /> </button>
           </div>
         </div>
 
         {/* Calendar Grid with Event Propagation */}
-        <section
-          className="w-full h-[20.4rem] mb-1 text-lg text-white grid grid-cols-7 grid-rows-7 gap-1 cursor-pointer"
-          onClick={handleDateClick}>
+        <section className="w-full h-[20.4rem] mb-1 text-lg text-white grid grid-cols-7 grid-rows-7 gap-1 cursor-pointer" onClick={handleDateClick}>
           {listOfDays.map((day) => (
-            <div key={day} className="p-1 flex items-center justify-center"> {day} </div>
-          ))}
+            <div key={day} className="p-1 flex items-center justify-center"> {day} </div> ))}
 
           {/* Previous Month Days */}
           {prevMonthDates.map((date, index) => (
-            <div key={`prev-${index}`} className={`${disableDayClass} ${flexClass}`}>{date}</div>
+            <div key={`prev-${index}`} className={`${disableDayClass} ${flexClass}`} > {date} </div>
           ))}
 
           {/* Current Month Days */}
-          {Array.from({ length: lastDay }, (_, i) => i + 1).map((date,index) => (
-            <div key={index}
-              className={`current-month rounded-full ${flexClass} aspect-square ${trackTodayDate(date)} hover:bg-[#27272a]`}>
-              {date}
-            </div>
-          ))}
+          {Array.from({ length: lastDay }, (_, i) => i + 1).map(
+            (date, index) => (
+              <div key={index} className={`current-month rounded-full ${flexClass} aspect-square ${trackTodayDate( date)} hover:bg-[#27272a]`}>
+                {date}
+              </div>
+            )
+          )}
 
           {/* Next Month Days */}
           {nextMonthDays.map((date, index) => (
-            <div key={`next-${index}`} className={`${disableDayClass} ${flexClass}`}>{date}</div>
-          ))}
+            <div key={`next-${index}`} className={`${disableDayClass} ${flexClass}`} > {date} </div> ))}
         </section>
       </section>
 
-      {/* Display Selected Date */} 
+      {/* Display Selected Date */}
       <div className="h-[80%] w-[90%] scroll border-y text-white mb-1  overflow-y-scroll hello">
-        <p>Selected Date: {selectedDate} {targetDate}</p> 
-
+        <p> Selected Date: {selectedDate} {targetDate}</p>
       </div>
-      <div className="h-[20%] w-full bg-[#212121] text-white "> hello</div>
-    </aside>
+      <div className="h-[20%] w-full bg-[#292D32] text-white "></div>
+    </>
   );
 };
-
 
 // #1E1F25
 
