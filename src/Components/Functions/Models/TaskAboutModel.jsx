e920517648640a0ca5278aa899fd8cd60b359c73
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-03-31 20:18:29 +0530

    refactor: Update TaskAboutModel to display dynamic task details and improve close model functionality

--- src/Components/Functions/Models/TaskAboutModel.jsx
+++ src/Components/Functions/Models/TaskAboutModel.jsx
@@ -5,14 +5,15 @@ export const AboutModel = ()=>{
     const { isShowInfoId, setInfoId, taskArr } = useContext(ToDoContext);
     const { isInfoOpen,setInfoOpen} = useContext(FormDataContext);
     const closeModel = ()=>{
-        setInfoOpen((open)=> !open)
+        setInfoOpen(false)
         setInfoId(null)
     }
-    const findTask = taskArr.find((task)=> task.id === isShowInfoId)
-    const {}
+    const findTask = isShowInfoId && taskArr.find((task)=> task.id === isShowInfoId)
+    const {content, checked, createdDateForform, description,group} = findTask || ''
+    
     return(
         <>
-            {isInfoOpen && isShowInfoId &&<div className="w-full h-full bg-[#010101] absolute top-0 left-0 text-white grid grid-cols-1 grid-rows-[8%_70%_22%]">
+            {isInfoOpen && isShowInfoId && <div className="w-full h-full bg-[#010101] absolute top-0 left-0 text-white grid grid-cols-1 grid-rows-[8%_70%_22%]">
                 <div className="m-2  flex justify-between items-center ">
                     <p className="pb-1 text-3xl border-b">About</p>
                     <button onClick={()=> closeModel()} className="p-2 text-2xl rounded-full hover:text-white/80 hover:bg-neutral-700"><RxCross1 /></button>
@@ -21,11 +22,11 @@ export const AboutModel = ()=>{
                 <section>
                 <div>
                     <p className="m-2 py-2 text-xl">Task</p>
-                    <p className="m-2 p-2 text-xl bg-[#1A1A1A] border border-neutral-600 rounded ">hello</p>
+                    <p className="m-2 p-2 text-xl bg-[#1A1A1A] border border-neutral-600 rounded ">{content}</p>
                 </div>
                 <div>
                     <p className="m-2 py-2 text-xl">Category</p>
-                    <p className="m-2 mb-4 p-2 text-xl bg-[#1A1A1A] border border-neutral-600 rounded ">hello</p>
+                    <p className="m-2 mb-4 p-2 text-xl bg-[#1A1A1A] border border-neutral-600 rounded ">{group}</p>
                 </div>
                 <div className="m-2  py-3 text-xl border-y grid grid-cols-[30%_10%_60%] grid-rows-2 gap-y-2">
                     <p className="">Priority</p>
@@ -36,16 +37,16 @@ export const AboutModel = ()=>{
                     <p className="">Status</p>
                     <span>:</span>
                     <span className="flex items-center gap-2 ">
-                        <p className="p-1 px-2 rounded bg-yellow-700/30 text-yellow-200 text-lg ">Pending</p>
+                        <p className="p-1 px-2 rounded bg-yellow-700/30 text-yellow-200 text-lg ">{!checked? 'Pending' : 'Completed'}</p>
                     </span>
                     <p className="">Created</p>
                     <span>:</span>
-                    <p>date</p>
+                    <p>{createdDateForform}</p>
                 </div>
 
                 <div>
                     <p className="m-2 py-2 text-xl">Description</p>
-                    <p className="m-2 p-2 text-xl bg-[#1A1A1A] border border-neutral-600 rounded ">hello</p>
+                    <p className="m-2 p-2 text-xl bg-[#1A1A1A] border border-neutral-600 rounded ">{description}</p>
                 </div>
                 </section>
                 
