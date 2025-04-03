09046979f9073b2725316521b54f5fd40baf0591
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-04-03 22:29:19 +0530

    refactor: Remove unused imports and simplify layout component selection in RouterCompo for cleaner code

--- src/Router/RouterCompo.jsx
+++ src/Router/RouterCompo.jsx
@@ -3,17 +3,17 @@ import { TodoApp } from '../To-Do/ToDoApp'
 import AppLayout from '../AppLayout'
 import { TaskActionItem } from '../To-Do/CompletedTask'
 import { CalenderComponent } from '../Components/layout/Calender/Calender'
-import useIsMobile from '../Components/Functions/UseIsMobile'
-import MobileComponent from '../Dashborad/MobileDashBoard'
+// import useIsMobile from '../Components/Functions/UseIsMobile'
+// import MobileComponent from '../Dashborad/MobileDashBoard'
 
  function RouterCompo() {
-  const isMobile = useIsMobile(570);  // Check mobile screen width
-  const LayoutComponent = isMobile ? <MobileComponent /> : <AppLayout />;  // Store JSX in a variable
+  // const isMobile = useIsMobile(570);  // Check mobile screen width
+  // const LayoutComponent = isMobile ? <MobileComponent /> : <AppLayout />;  // Store JSX in a variable
   
   const router = createBrowserRouter([
     {
       path: '/',
-      element: LayoutComponent,  // Use computed JSX here
+      element: <AppLayout />,  // Use computed JSX here
       children: [
         { path: '/', element: <TodoApp /> },
         { path: '/completedTask', element: <TaskActionItem /> },
