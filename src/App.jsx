4010c93c109f03ea10edee912446996b5527227b
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-04-30 00:53:53 +0530

    refactor: remove TaskManager components and related styles
    - Deleted TaskHeader, TaskList, TaskManager, TaskSection components.
    - Removed AddTaskForm and associated CSS.
    - Cleaned up router configuration and removed unused assets.
    - Eliminated index.css and main.jsx files as part of the refactor.
    - Removed unnecessary imports and context references.

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import RouterCompo from "./Router/RouterCompo";
-function App() {
-  return <RouterCompo/>
-  
-}
-
-export default App;
--- src/Contexts/CreateContext.jsx
+++
@@ -1,6 +0,0 @@
-// src/Contexts/ToDoContext.js
-import { createContext } from "react";
-
-export const ToDoContext = createContext();
-export const FormDataContext = createContext();
-export const DateContext = createContext()
