caf43d215b88a83cefbc140580d968a0e3445a09
debanjan-bera <debanjan.bera25discord@gmail.com>
2025-05-02 22:50:30 +0530

    fix: update user page to correctly retrieve and display user ID

--- client/src/app/dashboard/users/[id]/page.jsx
+++ client/src/app/dashboard/users/[id]/page.jsx
@@ -1,12 +1,30 @@
 import Link from 'next/link'
 import React from 'react'
 
-export default function Page({ params }) {
+export default async function Page({ params }) {
+
+  const { id } = await params
   return (
     <>
     
-    <div>usersData page : {params.id}</div>
-    <Link href={"/"}>Home page </Link>
+    <div>usersData page : {id}</div>
+      <Link href="/">Home page</Link>
+
     </>
   )
 }
+
+
+// 'use client';
+// import { useParams } from 'next/navigation';
+
+// export default function Page() {
+//   const params = useParams();
+
+//   return (
+//     <>
+//       <div>usersData page : {params.id}</div>
+//       <Link href="/">Home page</Link>
+//     </>
+//   );
+// }
